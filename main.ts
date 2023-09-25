1890e546b9073117f3054ce23764bbc725997ebb
VictorBaril <146025520+VictorBaril@users.noreply.github.com>
2023-09-25 15:09:43 -0400

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,6 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, Player, Render.getAttribute(Render.attribute.dirX), 0)
+	
 })
-let projectile: Sprite = null
-let Player: Sprite = null
-Player = Render.getRenderSpriteVariable()
+let Player = Render.getRenderSpriteVariable()
 tiles.setCurrentTilemap(tilemap`niveau1`)
 Render.setViewMode(ViewMode.raycastingView)
