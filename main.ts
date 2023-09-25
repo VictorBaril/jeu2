dc04fdea2d7a8d69979a8f82010842655e4b8c8d
VictorBaril <146025520+VictorBaril@users.noreply.github.com>
2023-09-25 15:11:01 -0400

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
 let Player = Render.getRenderSpriteVariable()
 tiles.setCurrentTilemap(tilemap`niveau1`)
 Render.setViewMode(ViewMode.raycastingView)
